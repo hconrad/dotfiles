e311d44f938d36fd8b7bb76597bfeb454698436b
Hans Conrad <hans.conrad@gmail.com>
2022-07-21 05:37:59 -0700

    Configured Treemacs

--- init.el
+++ init.el
@@ -55,7 +55,8 @@
                 term-mode-hook
 		vterm-mode-hook
                 shell-mode-hook
-                eshell-mode-hook))
+                eshell-mode-hook
+		treemacs-mode-hook))
   (add-hook mode (lambda () (display-line-numbers-mode 0))))
 
 
@@ -184,6 +185,7 @@
   (interactive)
   (vterm))
 
+
 (use-package general
   :config
  (general-create-definer my/leader-keys 
@@ -198,22 +200,23 @@
   :states 'normal
   :keymaps '(cider-repl-mode-map)
   :prefix ",")
-  (my/leader-keys
-    "SPC" '(counsel-M-x :which-key "Execute")
-    "pt" '(projectile-dired :which-key "Dired")
-    "/" '(counsel-projectile-ag :which-key "Search Project")
-    ":" '(eval-expression :which-key "Evaluate Expr")
-    ";" '(open-terminal :which-key "Terminal")
-    "e" '(:ignore t :which-key "Emacs")
-    "ef" '(eval-defun :which-key "Eval defun")
-    "eb" '(eval-buffer :which-key "Eval buffer")
-    "t"  '(:ignore t :which-key "toggles")
-    "tt" '(counsel-load-theme :which-key "choose theme")
-    "f" '(:ignore t :which-key "Files")
-    "fs" '(save-buffer :which-key "Save")
-    "v" '(:ignore t :which-key "View")
-    "vs" '(window-configuration-to-register :which-key "Store View")
-    "vl" '(jump-to-register :which-key "Load View")))
+
+(my/leader-keys
+  "SPC" '(counsel-M-x :which-key "Execute")
+  "/" '(counsel-projectile-ag :which-key "Search Project")
+  ";" '(open-terminal :which-key "Terminal")
+  ":" '(eval-expression :which-key "Evaluate Expr")
+  "e" '(:ignore t :which-key "Emacs")
+  "ef" '(eval-defun :which-key "Eval defun")
+  "eb" '(eval-buffer :which-key "Eval buffer")
+  "t"  '(:ignore t :which-key "Treemacs")
+  "tt" '(treemacs :which-key "Toggle" )
+  "vt" '(counsel-load-theme :which-key "choose theme")
+  "f" '(:ignore t :which-key "Files")
+  "fs" '(save-buffer :which-key "Save")
+  "v" '(:ignore t :which-key "View")
+  "vs" '(window-configuration-to-register :which-key "Store View")
+  "vl" '(jump-to-register :which-key "Load View")))
 
 (my/leader-keys
   "o" '(:ignore t :which-key "Org Roam")
@@ -224,7 +227,29 @@
   "oc" '(org-roam-capture :which-key "Capture")
   "ot" '(org-roam-dailies-capture-today :which-key "Capture Today"))
 
+;; Treemacs
+(use-package treemacs
+  :ensure t
+  :defer t
+  :general
+  (my/leader-keys
+    :states '(normal)
+    "tt" '(treemacs :which-ke "treemacs") 
+    "0" '(treemacs-select-window :which-key "Select Treemacs")))
 
+(use-package treemacs-evil
+  :after (treemacs evil)
+  :ensure t)
+
+(use-package treemacs-projectile
+  :after (treemacs projectile)
+  :ensure t)
+
+(use-package treemacs-magit
+  :after (treemacs magit)
+  :ensure t)
+
+;; End Treemacs
 
 (use-package counsel
   :bind (("M-x" . counsel-M-x)
@@ -474,121 +499,22 @@ Send Region to Repl."
 
 
 (add-hook 'clojure-mode-hook #'smartparens-mode)
-;;Zprint
-(load-file "/home/hans/repos/zprint.el/zprint.el")
-(add-hook 'clojure-mode-hook #'zprint-mode)
-;;TREEMACS
-(use-package treemacs
-  :ensure t
-  :defer t
-  :init
-  (with-eval-after-load 'winum
-    (define-key winum-keymap (kbd "M-0") #'treemacs-select-window))
-  :config
-  (progn
-    (setq treemacs-collapse-dirs                   (if treemacs-python-executable 3 0)
-          treemacs-deferred-git-apply-delay        0.5
-          treemacs-directory-name-transformer      #'identity
-          treemacs-display-in-side-window          t
-          treemacs-eldoc-display                   'simple
-          treemacs-file-event-delay                5000
-          treemacs-file-extension-regex            treemacs-last-period-regex-value
-          treemacs-file-follow-delay               0.2
-          treemacs-file-name-transformer           #'identity
-          treemacs-follow-after-init               t
-          treemacs-expand-after-init               t
-          treemacs-find-workspace-method           'find-for-file-or-pick-first
-          treemacs-git-command-pipe                ""
-          treemacs-goto-tag-strategy               'refetch-index
-          treemacs-header-scroll-indicators        '(nil . "^^^^^^")'
-          treemacs-hide-dot-git-directory          t
-          treemacs-indentation                     2
-          treemacs-indentation-string              " "
-          treemacs-is-never-other-window           nil
-          treemacs-max-git-entries                 5000
-          treemacs-missing-project-action          'ask
-          treemacs-move-forward-on-expand          nil
-          treemacs-no-png-images                   nil
-          treemacs-no-delete-other-windows         t
-          treemacs-project-follow-cleanup          nil
-          treemacs-persist-file                    (expand-file-name ".cache/treemacs-persist" user-emacs-directory)
-          treemacs-position                        'left
-          treemacs-read-string-input               'from-child-frame
-          treemacs-recenter-distance               0.1
-          treemacs-recenter-after-file-follow      nil
-          treemacs-recenter-after-tag-follow       nil
-          treemacs-recenter-after-project-jump     'always
-          treemacs-recenter-after-project-expand   'on-distance
-          treemacs-litter-directories              '("/node_modules" "/.venv" "/.cask")
-          treemacs-show-cursor                     nil
-          treemacs-show-hidden-files               t
-          treemacs-silent-filewatch                nil
-          treemacs-silent-refresh                  nil
-          treemacs-sorting                         'alphabetic-asc
-          treemacs-select-when-already-in-treemacs 'move-back
-          treemacs-space-between-root-nodes        t
-          treemacs-tag-follow-cleanup              t
-          treemacs-tag-follow-delay                1.5
-          treemacs-text-scale                      nil
-          treemacs-user-mode-line-format           nil
-          treemacs-user-header-line-format         nil
-          treemacs-wide-toggle-width               70
-          treemacs-width                           35
-          treemacs-width-increment                 1
-          treemacs-width-is-initially-locked       t
-          treemacs-workspace-switch-cleanup        nil)
 
-    ;; The default width and height of the icons is 22 pixels. If you are
-    ;; using a Hi-DPI display, uncomment this to double the icon size.
-    ;;(treemacs-resize-icons 44)
+(load "~/repos/zprint.el/zprint.el" )
+(add-hook 'clojure-mode-hook 'zprint-mode)
+;; NOTE: Make sure to configure a GitHub token before using this package!
+;; - https://magit.vc/manual/forge/Token-Creation.html#Token-Creation
+;; - https://magit.vc/manual/ghub/Getting-Started.html#Getting-Started
 
-    (treemacs-follow-mode t)
-    (treemacs-filewatch-mode t)
-    (treemacs-fringe-indicator-mode 'always)
-    (when treemacs-python-executable
-      (treemacs-git-commit-diff-mode t))
-
-    (pcase (cons (not (null (executable-find "git")))
-                 (not (null treemacs-python-executable)))
-      (`(t . t)
-       (treemacs-git-mode 'deferred))
-      (`(t . _)
-       (treemacs-git-mode 'simple)))
-
-    (treemacs-hide-gitignored-files-mode nil))
-  :bind
-  (:map global-map
-        ("M-0"       . treemacs-select-window)
-        ("C-x t 1"   . treemacs-delete-other-windows)
-        ("C-x t t"   . treemacs)
-        ("C-x t d"   . treemacs-select-directory)
-        ("C-x t B"   . treemacs-bookmark)
-        ("C-x t C-t" . treemacs-find-file)
-        ("C-x t M-t" . treemacs-find-tag)))
-
-(use-package treemacs-evil
-  :after (treemacs evil)
-  :ensure t)
-
-(use-package treemacs-projectile
-  :after (treemacs projectile)
-  :ensure t)
-
-(use-package treemacs-icons-dired
-  :hook (dired-mode . treemacs-icons-dired-enable-once)
-  :ensure t)
-
-(use-package treemacs-magit
-  :after (treemacs magit)
-  :ensure t)
-;;END Treemacs
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("cbdf8c2e1b2b5c15b34ddb5063f1b21514c7169ff20e081d39cf57ffee89bc1e" "66bdbe1c7016edfa0db7efd03bb09f9ded573ed392722fb099f6ac6c6aefce32" "97db542a8a1731ef44b60bc97406c1eb7ed4528b0d7296997cbb53969df852d6" "f91395598d4cb3e2ae6a2db8527ceb83fed79dbaf007f435de3e91e5bda485fb" "5784d048e5a985627520beb8a101561b502a191b52fa401139f4dd20acb07607" default))
  '(package-selected-packages
-   '(all-the-icons flx org-roam org-modern cider winum ace-window forge evil-magit magit counsel-projectile projectile which-key use-package rainbow-delimiters ivy-rich hydra helpful general evil-collection doom-themes doom-modeline counsel command-log-mode)))
+   '(eglot prettier vue-mode lsp-ui markdownfmt treemacs-magit treemacs-projectile treemacs-evil treemacs exec-path-from-shell lsp-mode all-the-icons flx org-roam org-modern cider winum ace-window forge evil-magit magit counsel-projectile projectile which-key use-package rainbow-delimiters ivy-rich hydra helpful general evil-collection doom-themes doom-modeline counsel command-log-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
